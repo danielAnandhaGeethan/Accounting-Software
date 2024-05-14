321cdc699ae079529aadee99191f0893837a5847
danielAnandhaGeethan <dj9759197591@gmail.com>
2024-05-14 22:50:31 +0530

    sample commit

--- app.py
+++ app.py
@@ -4,8 +4,30 @@ import csv
 import matplotlib.pyplot as plt
 import seaborn as sns
 import warnings
+import pymongo
+
+username = "Daniel"
+password = "20bai1111"
+dbname = "accounting-software"
+
+connection_string = f"mongodb+srv://{username}:{password}@cluster0.bk1vhhz.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+
+client = pymongo.MongoClient(connection_string)
+db = client[dbname]
+
+def connect_mongodb():
+    
+
+    """collection = db["hello"]
+
+    print("hello")
+    for document in collection.find():
+        print(document)
+
+    client.close()"""
 
 def main():
+
     st.sidebar.markdown("<h1 style='backgrouond-color: #000000; color: #ffffff; font-size: 30px;'>FinovoTech</h1>", unsafe_allow_html = True)
     menu_options = ["Dashboard", "Purchase Order", "Sales", "Invoices", "Taxes", "Expenses"]
 
@@ -44,7 +66,7 @@ def total_sales(data):
     sales = 0
 
     for index,row in data.iterrows():
-        sales += int(row['total sales'])
+        sales += int(row['total_sales'])
 
     return sales
 
@@ -82,10 +104,10 @@ def show_recent_orders(data):
     if not show_all:
         top_three = data.head(3)
         for index, row in top_three.iterrows():
-            st.markdown(f"<div style='display: flex; flex-direction: row; justify-content: between;'><div style='display: flex; flex-direction: column; justify-content: start;'><div style='font-size: 17px'>{row['purchase order']}</div><h6 style='font-size: 14px'>{row['Date'].strftime('%Y-%m-%d')}</h6></div><div>${row['amount']}</div></div>", unsafe_allow_html = True)
+            st.markdown(f"<div style='display: flex; flex-direction: row; justify-content: between;'><div style='display: flex; flex-direction: column; justify-content: start;'><div style='font-size: 17px'>{row['purchase_order']}</div><h6 style='font-size: 14px'>{row['Date'].strftime('%Y-%m-%d')}</h6></div><div>${row['amount']}</div></div>", unsafe_allow_html = True)
     else:
         for index, row in data.iterrows():
-            st.markdown(f"<div style='display: flex; flex-direction: row; justify-content: between;'><div style='display: flex; flex-direction: column; justify-content: start;'><div style='font-size: 17px'>{row['purchase order']}</div><h6 style='font-size: 14px'>{row['Date'].strftime('%Y-%m-%d')}</h6></div><div>${row['amount']}</div></div>", unsafe_allow_html = True)
+            st.markdown(f"<div style='display: flex; flex-direction: row; justify-content: between;'><div style='display: flex; flex-direction: column; justify-content: start;'><div style='font-size: 17px'>{row['purchase_order']}</div><h6 style='font-size: 14px'>{row['Date'].strftime('%Y-%m-%d')}</h6></div><div>${row['amount']}</div></div>", unsafe_allow_html = True)
 
 def show_product_selling(data):
 
@@ -100,10 +122,10 @@ def show_product_selling(data):
         st.table(data)
 
 def show_selling_category(data):
-    product_group = data.groupby('product name')['total sales'].sum().reset_index()
+    product_group = data.groupby('product_name')['total_sales'].sum().reset_index()
 
-    total_sales_all_products = product_group['total sales'].sum()
-    product_group['percentage'] = (product_group['total sales'] / total_sales_all_products) * 100
+    total_sales_all_products = product_group['total_sales'].sum()
+    product_group['percentage'] = (product_group['total_sales'] / total_sales_all_products) * 100
     product_group = product_group.sort_values(by='percentage', ascending=False)
 
     st.markdown("<h6 style='font-size: 20px'>Top Selling Products</h6>", unsafe_allow_html=True)
@@ -113,12 +135,74 @@ def show_selling_category(data):
     if not show_all:
         top_three = product_group.head(3)
         for index, row in top_three.iterrows():
-            st.write(row['product name'], f"{row['percentage']:.2f}%")
+            st.write(row['product_name'], f"{row['percentage']:.2f}%")
             progress_bar = st.progress(int(row['percentage']))
     else:
         for index, row in product_group.iterrows():
-            st.write(row['product name'], f"{row['percentage']:.2f}%")
+            st.write(row['product_name'], f"{row['percentage']:.2f}%")
             progress_bar = st.progress(int(row['percentage']))
+
+def show_purchase_status(data):
+
+    st.subheader('Status Distribution')
+    status_counts = data['status'].value_counts()
+
+    fig_status, ax_status = plt.subplots(figsize=(5, 5))
+    ax_status.pie(status_counts.values, labels=status_counts.index, autopct='%1.1f%%', startangle=140)
+    ax_status.axis('equal')
+    st.pyplot(fig_status)
+    plt.close(fig_status)
+
+def show_stock_status(data):
+
+    status_counts = data['status'].value_counts()
+
+    st.subheader('Status Distribution')
+
+    fig, ax = plt.subplots(figsize=(8, 6))
+    ax.bar(status_counts.index, status_counts.values, color='skyblue')
+    ax.set_xlabel('Status')
+    ax.set_ylabel('Count')
+    ax.set_title('Status Distribution')
+    ax.tick_params(axis='x', rotation=45) 
+    plt.tight_layout() 
+    st.pyplot(fig)
+    plt.close(fig)
+
+def convert_to_dataframe(collection):
+
+    cursor = collection.find({}, {"_id": 0})
+    df = pd.DataFrame(list(cursor))
+
+    return df
+
+def show_dashboard():
+    st.markdown("<h1 style='color: #ffffff; font-size: 30px;'>DashBoard</h1>", unsafe_allow_html = True)
+
+    warnings.filterwarnings("ignore", category=UserWarning)
+
+    data = convert_to_dataframe(db["Purchases"])
+    sales = convert_to_dataframe(db["Sales"])
+
+    sales_amount = total_sales(sales)
+    amount,delivered = total_amount(data)
+
+    row_1 = st.columns(3)
+
+    with row_1[0]:
+        st.error(f"Total Sales \n\n {sales_amount}")
+
+    with row_1[1]:
+        st.info(f"Total Purchase Amount \n\n {amount}")
+
+    with row_1[2]:
+        st.success(f"Total Delivered \n\n {delivered}")
+
+    with st.columns([1,5,1])[1]:
+        show_purchase_status(data)
+
+    with st.columns([1,5,1])[1]:
+        show_stock_status(sales)
 
 def show_purchase_order():
     
@@ -178,74 +262,18 @@ def show_purchase_order():
                     "amount": amount
                 }
 
-                with open("PurchaseOrder.csv", "a", newline="") as file:
-                    writer = csv.DictWriter(file, fieldnames=new_data.keys())
-                    writer.writerow(new_data)
+                collection = db["Purchases"]
+                collection.insert_one(new_data)
 
     else:
-        data = pd.read_csv('./PurchaseOrder.csv')
+        data = convert_to_dataframe(db["Purchases"])
         st.write(data)
-
-def show_purchase_status(data):
-
-    st.subheader('Status Distribution')
-    status_counts = data['status'].value_counts()
-
-    fig_status, ax_status = plt.subplots(figsize=(5, 5))
-    ax_status.pie(status_counts.values, labels=status_counts.index, autopct='%1.1f%%', startangle=140)
-    ax_status.axis('equal')
-    st.pyplot(fig_status)
-    plt.close(fig_status)
-
-def show_stock_status(data):
-
-    status_counts = data['status'].value_counts()
-
-    st.subheader('Status Distribution')
-
-    fig, ax = plt.subplots(figsize=(8, 6))
-    ax.bar(status_counts.index, status_counts.values, color='skyblue')
-    ax.set_xlabel('Status')
-    ax.set_ylabel('Count')
-    ax.set_title('Status Distribution')
-    ax.tick_params(axis='x', rotation=45) 
-    plt.tight_layout() 
-    st.pyplot(fig)
-    plt.close(fig)
-
-def show_dashboard():
-    st.markdown("<h1 style='color: #ffffff; font-size: 30px;'>DashBoard</h1>", unsafe_allow_html = True)
-
-    warnings.filterwarnings("ignore", category=UserWarning)
-
-    data = pd.read_csv('./PurchaseOrder.csv')
-    sales = pd.read_csv('./Sales.csv')
-
-    sales_amount = total_sales(sales)
-    amount,delivered = total_amount(data)
-
-    row_1 = st.columns(3)
-
-    with row_1[0]:
-        st.error(f"Total Sales \n\n {sales_amount}")
-
-    with row_1[1]:
-        st.info(f"Total Purchase Amount \n\n {amount}")
-
-    with row_1[2]:
-        st.success(f"Total Delivered \n\n {delivered}")
-
-    with st.columns([1,5,1])[1]:
-        show_purchase_status(data)
-
-    with st.columns([1,5,1])[1]:
-        show_stock_status(sales)
 
 def show_sales():
     st.markdown("<h1 style='color: #ffffff; font-size: 40px;'>Sales</h1>", unsafe_allow_html = True)
     
-    purchases = pd.read_csv('./PurchaseOrder.csv')
-    sales = pd.read_csv('./Sales.csv')
+    purchases = convert_to_dataframe(db["Purchases"])
+    sales = convert_to_dataframe(db["Sales"])
 
     sales_count = total_sales(sales)
 
@@ -274,7 +302,7 @@ def show_sales():
 def show_invoices():
     st.markdown("<h1 style='color: #ffffff; font-size: 40px;'>Invoices</h1>", unsafe_allow_html = True)
     
-    invoices = pd.read_csv('./Invoices.csv')
+    invoices = convert_to_dataframe(db["Invoices"])
 
     st.table(invoices)
 
